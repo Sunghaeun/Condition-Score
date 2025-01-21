2c1bbcb0faf94e3059fb78fb68190d0a9d858db2
Haeun <hahashe@handong.ac.kr>
2025-01-21 17:24:21 +0900

    modal 내용 수정

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,38 @@
-import React from 'react'
 import { useNavigate } from "react-router-dom";
 
+import React, { useState } from 'react';
+import Modal from "./Modal"
+
 function Home() {
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
     const navigate = useNavigate();
     function onClickBtn() {
-        navigate('/add');
+        navigate('/add'); 
     }
   return (
     <div>
+
+      
+<button onClick={openModal}>모달팝업</button>
+      <Modal open={modalOpen} close={closeModal} > 
+      <p>안녕하십니까!
+      </p>
+      </Modal>
+
         <button type="button" onClick={onClickBtn}>
             Add
         </button>
+
+        
     </div>
   )
 }
