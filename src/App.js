0cabe482de49009b6d9e32f39236db4886b61640
Gyuho-Han <0203ho@naver.com>
2025-01-21 16:56:55 +0900

    modal과 merge 및 에러수정

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import styles from './App.module.css';
+
 import Forms from './components/Forms.js';
 import Home from './components/Home.js';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import Modal from "./components/Modal"
 
 
 function App() {
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   return (
-    <>
+    <div>
+      <h2>몸과 마음의 점수</h2>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/add" element={<Forms />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,89 @@
-import React from 'react'
-import { useNavigate } from "react-router-dom";
+import {React,  useEffect, useState } from 'react'
+import { useNavigate} from "react-router-dom";
+import axios from "axios";
+import $ from "jquery";
+import Modal from "./Modal";
+import styles from "../style/modal.css";
+
 
 function Home() {
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const openModal = () => {
+        setModalOpen(true);
+    };
+    const closeModal = () => {
+        setModalOpen(false);
+    };
+
     const navigate = useNavigate();
     function onClickBtn() {
-        navigate('/add');
+        navigate('/add'); 
     }
-  return (
-    <div>
-        <button type="button" onClick={onClickBtn}>
-            Add
-        </button>
+
+    function onClickDeleteBtn() {
+        deleteAll();
+        //window.location.reload();
+    }
+
+    function getDataFromJSONFile() {
+          axios.get("https://678f220a49875e5a1a90a2cf.mockapi.io/conditions")
+            .then((response) => {
+                const scores = response.data;
+                console.log(scores);
+                scores.forEach((item) => {
+                    $("#responseList").append(`
+                          <div class="reponse-list">
+                            <span class="bodyScore">${item.bodyScore}</span>
+                            <span class="bodyReason">${item.bodyReason}</span>
+                            <span class="emoScore">${item.emoScore}</span>
+                            <span class="emoReason">${item.emoReason}</span>
+                          </div>
+                        `);
+                  });
+            })
+            .catch((error) => {
+                console.error("에러: ", error);
+            });
+      }
+
+      function deleteAll() {
+        axios.get("https://678f220a49875e5a1a90a2cf.mockapi.io/conditions")
+            .then((response) => {
+                const scores = response.data;
+                console.log(scores);
+                scores.forEach((item) => {
+                    let deleteID = item.id;
+                    console.log("deleteId" + deleteID);
+                    axios.delete("https://678f220a49875e5a1a90a2cf.mockapi.io/conditions/" + deleteID)
+                    .catch(error => {
+                            console.error("Error:", error);
+                        });
+                  });
+            });
+      }
+
+    window.onload = useEffect(() => {
+        getDataFromJSONFile();
+    }, []);
+        
+    return (
+        <div>
+            <div id="responseList"></div>
+            <button type="button" onClick={onClickBtn}>
+                Add
+            </button>
+            <button type="button" onClick={onClickDeleteBtn}>
+                Delete
+            </button>
+
+        <button onClick={openModal}>모달팝업</button>
+        {/* //header 부분에 텍스트를 입력한다. */}
+        <Modal open={modalOpen} close={closeModal} header="Modal heading"> 
+        <form className={styles.form_group}>
+          <button className={styles.submitButton} type="submit">제출</button>
+        </form>
+        </Modal>
     </div>
   )
 }
