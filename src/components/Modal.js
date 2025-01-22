d1b8d94d52997b10f96f4e2e3244931a8399f17d
Haeun <hahashe@handong.ac.kr>
2025-01-22 10:40:33 +0900

    modal 코드 오류 수정

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,11 @@
+Modal.js
+
 import React from 'react';
 import "../style/modal.css";
 
 const Modal = (props) => {
   // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
-  const { open, close, header } = props;
+  const { open, close} = props;
 
   return (
     // 모달이 열릴때 openModal 클래스가 생성된다.
@@ -11,12 +13,6 @@ const Modal = (props) => {
       {open ? (
         <section>
           <header>
-            {header}
-            <button className="close" onClick={close}>
-              &times;
-            </button>
-          </header>
-          <main>{props.children}</main>
             <div className='modalHeaderBox'>
               <div className='modalProfilePic'> </div>
               <h2 className='modalProfileName'>성하은</h2>
